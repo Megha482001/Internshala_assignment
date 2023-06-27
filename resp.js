fd3c9884ccccd10d7aed1426e1f3bd4258d88564
Megha Sarkar <109139896+Megha482001@users.noreply.github.com>
2023-06-27 22:13:57 +0530

    added index.html , style.css , resp.js

---
+++ resp.js
@@ -0,0 +1,14 @@
+burger = document.querySelector('.burger')
+navbar = document.querySelector('.navbar')
+navList = document.querySelector('.nav-List')
+rightNav = document.querySelector('.rightNav')
+
+
+
+
+burger.addEventListener('click', ()=>{
+rightNav.classList.toggle('v-class-resp');
+nav-List.classList.toggle('v-class-resp');
+navbar.classList.toggle('h-nav-resp')
+
+})
